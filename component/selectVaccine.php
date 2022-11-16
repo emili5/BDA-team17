7a4933d5f5f686f2252319235474c50d998a99dd
jh-jin <devjin0122@gmail.com>
2022-11-17 00:34:59 +0900

    Feat: selectVaccine

---
+++ component/selectVaccine.php
@@ -0,0 +1,33 @@
+<?php
+    include "../config.php"
+?>
+
+<?php
+	$userId = 3;
+	$x = 127.123456;
+	$y = 37.123456;
+	//$x = userLocation['x'];
+	//$y = userLocation['y'];
+	//$userId = $_SESSION['userId'];
+	
+	$sql = "SELECT name
+			FROM Vaccine";
+
+	$res = mysqli_query($mysqli, $sql);
+	
+	// 콤보박스 만들기
+	printf("<select name='category'>");
+	if ($res) {
+		while ($row = mysqli_fetch_array($res, MYSQLI_ASSOC)) {
+			$name = $row['name'];
+			printf("<option value='$name'>$name</option>");
+		}
+	}
+	else {
+		echo "Could not rank Record: $sql \n".mysqli_error($mysqli);
+  	}
+	printf("</select>");
+
+	mysqli_close($mysqli);
+?>
+
