1b8e722710557d10e11f68e3efdfb954979cd4bc
gkim <kimkm0419@naver.com>
2022-11-17 00:30:49 +0900

    #12 Feat : Create insert.sql

---
+++ DB_Script/insert.sql
@@ -0,0 +1,11 @@
+-- 'Sex' Table Initialization
+INSERT INTO Sex(type) VALUES('female')
+INSERT INTO Sex(type) VALUES('male')
+
+-- 'Category' Table Initialization
+
+-- 'Vaccine' Table Initialization
+
+-- 'Hospital' Table Initialization
+
+-- 'Price' Table Initialization
